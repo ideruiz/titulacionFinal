2fa8603c3e11794c3b795ec9a2179d5adb80293e
ideruiz <gerardo.id.ruiz@gmail.com>
2013-10-06 22:47:04 -0500

    Actulización
    Se reemplazaron las vistas de los exportables, agregué la carpeta de
    Excel a Clases y agregué un css de los "ver...php"

--- verCertificacion.php
+++ verCertificacion.php
@@ -1,170 +1,190 @@
 <html>
+
+<!--
+*@Sistema de titulación UAO
+*@versión: 3.0      
+*@modificado: 14 de Septiembre del 2013
+*@autor back-end: Saúl David López Delgado
+*@autor front-end: Gerardo Ideyoshi Ruiz Ventura
+-->
+
 <head>
-	<title> Vista Previa - Titulacion</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	  <title> Vista Previa - Certificación</title>
+
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="language" content="es" />
 
     <link rel="stylesheet" type = "text/css" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="./estilos/style_verExportables.css">
+
 </head>
+
 <body>
-	<?php 
 
-		include_once("Clases/Conexion.php");
-		$conexion = new Conexion();
+  	<?php
+  		include_once("Clases/Conexion.php");
+  		$conexion = new Conexion();
 
-    function llenaCeros($str){
+      function llenaCeros($str){
 
-      if (strlen($str) == 2){
-        return $str;
-      } else{
-        return '0'.$str;
+        if (strlen($str) == 2){
+          return $str;
+        } else{
+          return '0'.$str;
+        }
       }
-    }
 
-	 ?>
+  	 ?>
 
-	<div style="overflow:scroll; ">
-		<table class="table table-hover" style="width:auto;">
-              <thead>
-                <tr>
-                  <th>CURP</th>
-                  <th>Apellido Paterno</th>
-                  <th>Apellido Materno</th>
-                  <th>Nombre</th>
-                  <th>Identificador del estado de la Institución</th>
-                  <th>Consecutivo de la Institución</th>
-                  <th>Identificador del área de la Carrera</th>
-                  <th>Identificador de la sub área de la Carrera</th>
-                  <th>Identificador del nivel de la Carrera</th>
-                  <th>Consecutivo de la Carrera</th>
-                  <th>Tipo de registro</th>
-                  <th>Sexo</th>
-                  <th>Entidad de Nacimiento</th>
-                  <th>Fecha de Nacimiento</th>
-                  <th>Fecha de examen profesional</th>
-                  <th>País donde radica el profesionista</th>
-                  <!-- Campos para postgrados
-                    <th>extra 1</th>
-                    <th>extra 2</th>
-                  -->
-                  <th></th>
+      <div id="todo">
 
-                </tr>
-              </thead>
-              <tbody>
+          <div id="btn-descargar">  
+              <input class="btn btn-large btn-primary" type="button" value="Descargar" onclick="location.href='Exportables/exportableCertificacion.csv';">
+          </div>
 
-                	<?php  
-                   /**
-                    * @param    string   Obtiene el mes
-                    * @return   string   Regrea el valor del mes como numero.
-                    */
-                    function convierteMes($m){
-                          switch($m){
-                            case "Enero":       $mes = '01'; return $mes; break;
-                            case "Febrero":     $mes = '02'; return $mes; break;
-                            case "Marzo":       $mes = '03'; return $mes; break;
-                            case "Abril":       $mes = '04'; return $mes; break;
-                            case "Mayo ":       $mes = '05'; return $mes; break;
-                            case "Junio":       $mes = '06'; return $mes; break;
-                            case "Julio":       $mes = '07'; return $mes; break;
-                            case "Agosto":      $mes = '08'; return $mes; break;
-                            case "Septiembre":  $mes = '09'; return $mes; break;
-                            case "Octubre":     $mes = '10'; return $mes; break;
-                            case "Noviembre":   $mes = '11'; return $mes; break;
-                            case "Diciembre":   $mes = '12'; return $mes; break;
-                          }                          
-                      }
+      	  <div id="tabla">
+          		<table class="table table-hover" border = "1" style="background-color: #10151C; color:white;">
+                        <thead>
+                          <tr>
+                            <th>CURP</th>
+                            <th>Apellido Paterno</th>
+                            <th>Apellido Materno</th>
+                            <th>Nombre</th>
+                            <th>Identificador del estado de la Institución</th>
+                            <th>Consecutivo de la Institución</th>
+                            <th>Identificador del área de la Carrera</th>
+                            <th>Identificador de la sub área de la Carrera</th>
+                            <th>Identificador del nivel de la Carrera</th>
+                            <th>Consecutivo de la Carrera</th>
+                            <th>Tipo de registro</th>
+                            <th>Sexo</th>
+                            <th>Entidad de Nacimiento</th>
+                            <th>Fecha de Nacimiento</th>
+                            <th>Fecha de examen profesional</th>
+                            <th>País donde radica el profesionista</th>
+                            <!-- Campos para postgrados
+                              <th>extra 1</th>
+                              <th>extra 2</th>
+                            -->
+                            <th></th>
 
-              		$sql = "SELECT * FROM titulacion";
-              		$result = $conexion->consulta($sql);
+                          </tr>
+                        </thead>
+                        <tbody>
 
-              		while($row = mysqli_fetch_array($result)){                   
+                          	<?php  
+                             /**
+                              * @param    string   Obtiene el mes
+                              * @return   string   Regrea el valor del mes como numero.
+                              */
+                              function convierteMes($m){
+                                    switch($m){
+                                      case "Enero":       $mes = '01'; return $mes; break;
+                                      case "Febrero":     $mes = '02'; return $mes; break;
+                                      case "Marzo":       $mes = '03'; return $mes; break;
+                                      case "Abril":       $mes = '04'; return $mes; break;
+                                      case "Mayo ":       $mes = '05'; return $mes; break;
+                                      case "Junio":       $mes = '06'; return $mes; break;
+                                      case "Julio":       $mes = '07'; return $mes; break;
+                                      case "Agosto":      $mes = '08'; return $mes; break;
+                                      case "Septiembre":  $mes = '09'; return $mes; break;
+                                      case "Octubre":     $mes = '10'; return $mes; break;
+                                      case "Noviembre":   $mes = '11'; return $mes; break;
+                                      case "Diciembre":   $mes = '12'; return $mes; break;
+                                    }                          
+                                }
 
-                      /**
-                      * @var    certificacion     Separa la fecha de certificacion en un array.
-                      * La fecha viene en formato: Lugar_de_certificacion, a DD de MM de AAAA;
-                      */
-                      $certificacion = explode (" ",$row['certificacion']);
+                        		$sql = "SELECT * FROM titulacion";
+                        		$result = $conexion->consulta($sql);
 
-                      /**
-                      * @var    anioC             Se obtiene el elemento año del array.
-                      * @param  $certificación    Se extrae un elemento del array.
-                      */
-                      $anioC = array_pop($certificacion);
+                        		while($row = mysqli_fetch_array($result)){                   
 
-                      /**
-                      * @param  $certificación    Se extrae un elemento del array.
-                      */
-                      array_pop($certificacion);
+                                /**
+                                * @var    certificacion     Separa la fecha de certificacion en un array.
+                                * La fecha viene en formato: Lugar_de_certificacion, a DD de MM de AAAA;
+                                */
+                                $certificacion = explode (" ",$row['certificacion']);
 
-                      /**
-                      * @var    mes               Se obtiene en un String el elemento mes del array.
-                      * @param  $certificación    Se extrae un elemento del array.
-                      */
-                      $mes = array_pop($certificacion);
+                                /**
+                                * @var    anioC             Se obtiene el elemento año del array.
+                                * @param  $certificación    Se extrae un elemento del array.
+                                */
+                                $anioC = array_pop($certificacion);
 
-                      /**
-                      * @param  mes                Se envía a la funcion mes el string Mes.
-                      * @var    mesC               Se Obtiene el valor del mes en número.
-                      */
-                      $mesC = convierteMes($mes);
-                      array_pop($certificacion);
+                                /**
+                                * @param  $certificación    Se extrae un elemento del array.
+                                */
+                                array_pop($certificacion);
 
-                      /**
-                      * @var    diaC              Se obtiene el elemento dia del array.
-                      */
-                      $diaC = array_pop($certificacion);
+                                /**
+                                * @var    mes               Se obtiene en un String el elemento mes del array.
+                                * @param  $certificación    Se extrae un elemento del array.
+                                */
+                                $mes = array_pop($certificacion);
 
-                      /**
-                      * @var    cer               Es la fecha de certifcación en el formado AAAAMMDD
-                      */
-                      $cer =  $anioC.$mesC.$diaC;
-                      
-              				$consulta = "SELECT * FROM profesionista WHERE fk_alumno = $row[fk_alumno]";
-              				$res = $conexion->consulta($consulta);
-              				$titulacion = mysqli_fetch_array($res);
+                                /**
+                                * @param  mes                Se envía a la funcion mes el string Mes.
+                                * @var    mesC               Se Obtiene el valor del mes en número.
+                                */
+                                $mesC = convierteMes($mes);
+                                array_pop($certificacion);
 
-              				$sql_carrera = "SELECT * FROM carrera WHERE nombre_carrera = '$row[carrera]'";
-              				$result_carrera = $conexion->consulta($sql_carrera);
-              				$carrera = mysqli_fetch_array($result_carrera);
+                                /**
+                                * @var    diaC              Se obtiene el elemento dia del array.
+                                */
+                                $diaC = array_pop($certificacion);
 
-              				$sql_constantes = "SELECT i_estado_institucion,consecutivo_institucion,codigo_LicMaster FROM constantes";
-              				$result_constantes = $conexion->consulta($sql_constantes);
-              				$constantes = mysqli_fetch_array($result_constantes);
-              			ECHO "<tr>
-              					<td > $row[curp]</td>
-              					<td > $row[a_paterno]</td>
-              					<td > $row[a_materno]</td>
-              					<td > $row[nombre]</td>
+                                /**
+                                * @var    cer               Es la fecha de certifcación en el formado AAAAMMDD
+                                */
+                                $cer =  $anioC.$mesC.$diaC;
+                                
+                        				$consulta = "SELECT * FROM profesionista WHERE fk_alumno = $row[fk_alumno]";
+                        				$res = $conexion->consulta($consulta);
+                        				$titulacion = mysqli_fetch_array($res);
 
-              					<td > $constantes[i_estado_institucion]</td>
-              					<td > ".llenaCeros("$constantes[consecutivo_institucion]")."</td>
+                        				$sql_carrera = "SELECT * FROM carrera WHERE nombre_carrera = '$row[carrera]'";
+                        				$result_carrera = $conexion->consulta($sql_carrera);
+                        				$carrera = mysqli_fetch_array($result_carrera);
 
-              					<td > $carrera[i_area]</td>
-              					<td > ".llenaCeros("$carrera[i_subArea]")."</td>
-              					<td > $carrera[i_nivel]</td>              					
-              					<td > ".llenaCeros("$carrera[consecutivo]")."</td>
+                        				$sql_constantes = "SELECT i_estado_institucion,consecutivo_institucion,codigo_LicMaster FROM constantes";
+                        				$result_constantes = $conexion->consulta($sql_constantes);
+                        				$constantes = mysqli_fetch_array($result_constantes);
+                        			ECHO "<tr>
+                        					<td > $row[curp]</td>
+                        					<td > $row[a_paterno]</td>
+                        					<td > $row[a_materno]</td>
+                        					<td > $row[nombre]</td>
 
-              					<td > $constantes[codigo_LicMaster]</td>
-              					<td > $titulacion[sexo]</td>
-              					<td > $titulacion[entidad_nacimiento]</td>
-              					<td > $titulacion[fecha_nacimiento]</td>
-              					<td > $cer </td>
-              					<td > $titulacion[pais_radica]</td>
-                        <!--
-              					<td > - </td>
-              					<td > - </td>
-                        -->
-              				</tr>";
-              		}
-              		?>
+                        					<td > $constantes[i_estado_institucion]</td>
+                        					<td > ".llenaCeros("$constantes[consecutivo_institucion]")."</td>
 
-                
-              </tbody>
-            </table>
+                        					<td > $carrera[i_area]</td>
+                        					<td > ".llenaCeros("$carrera[i_subArea]")."</td>
+                        					<td > $carrera[i_nivel]</td>              					
+                        					<td > ".llenaCeros("$carrera[consecutivo]")."</td>
 
-	</div>
-	
+                        					<td > $constantes[codigo_LicMaster]</td>
+                        					<td > $titulacion[sexo]</td>
+                        					<td > $titulacion[entidad_nacimiento]</td>
+                        					<td > $titulacion[fecha_nacimiento]</td>
+                        					<td > $cer </td>
+                        					<td > $titulacion[pais_radica]</td>
+                                  <!--
+                        					<td > - </td>
+                        					<td > - </td>
+                                  -->
+                        				</tr>";
+                        		}
+                        		?>
+
+                          
+                        </tbody>
+                      </table>
+
+      	</div>
+	    </div>
 
 
 </body>
--- verTitulacion.php
+++ verTitulacion.php
@@ -1,84 +1,104 @@
 <html>
+
+<!--
+*@Sistema de titulación UAO
+*@versión: 3.0      
+*@modificado: 14 de Septiembre del 2013
+*@autor back-end: Saúl David López Delgado
+*@autor front-end: Gerardo Ideyoshi Ruiz Ventura
+-->
+
 <head>
-	<title> Vista Previa - Titulacion</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title> Vista Previa - Titulacion</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="language" content="es" />
 
     <link rel="stylesheet" type = "text/css" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="./estilos/style_verExportables.css">
+
 </head>
+
 <body>
-	<?php 
 
-		include_once("Clases/Conexion.php");
-		$conexion = new Conexion();
+  	<?php 
 
-    $count=0;
-	 ?>
+  		include_once("Clases/Conexion.php");
+  		$conexion = new Conexion();
 
-	<div style="overflow:scroll; ">
-		<table class="table table-hover" style="width:auto;">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Tipo</th>
-                  <th>Nombre</th>
-                  <th>Título</th>
-                  <th>Nombre del Rector</th>
-                  <th>Campus y Fecha de Expedición de títutlo</th>
-                  <th>Libro</th>
-                  <th>Foja</th>
-                  <th>CURP</th>
-                  <th>Estudios de Bachillerato</th>
-                  <th>Periodo</th>
-                  <th>Entidad Federativa</th>
-                  <th>Institución</th>
-                  <th>Carrera</th>
-                  <th>Periodo</th>
-                  <th>Entidad Federativa</th>
-                  <th>Fecha de Examen</th>
-                  <th>Lugar y fecha de la Certificación</th>
-                  <th>Administración Escolar</th>
+      $count=0;
+  	 ?>
 
-                </tr>
-              </thead>
-              <tbody>
+    <div id="todo">
 
-                <?php  
-              		$sql = "SELECT * FROM titulacion";
-              		$result = $conexion->consulta($sql);
+      <div id="btn-descargar">  
+          <input class="btn btn-large btn-primary" type="button" value="Descargar" onclick="location.href='Exportables/exportableTitulacion.xlsx';">
+      </div>
 
-              		while($row = mysqli_fetch_array($result)){
-                    $count = $count + 1;
-              			$nombre = $row['nombre']." ".$row['a_paterno']." ".$row['a_materno'];
-              			ECHO "<tr>
-              					<td > $count</td>
-              					<td > $row[tipo_titulacion]</td>
-              					<td > $nombre</td>
-              					<td > $row[titulo]</td>
-              					<td > $row[nombre_rector]</td>
-              					<td > $row[exp_titulo]</td>
-              					<td > $row[libro]</td>
-              					<td > $row[foja]</td>
-              					<td > $row[curp]</td>
-              					<td > $row[nombre_bachillerato]</td>
-              					<td > $row[periodo_bachillerato]</td>
-              					<td > $row[entidad_bachillerato]</td>
-              					<td > $row[institucion]</td>
-              					<td > $row[carrera]</td>
-              					<td > $row[periodo_carrera]</td>
-              					<td > $row[entidad_institucion]</td>
-              					<td > $row[examen_profesional]</td>
-              					<td > $row[certificacion]</td>
-              					<td > $row[administracion_escolar]</td>
-              				</tr>";
-              		}
-              	?>                
-              </tbody>
-            </table>
+	   <div id="tabla">
+      		<table class="table table-hover"  border = "1" style="background-color: #10151C; color:white;">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Tipo</th>
+                        <th>Nombre</th>
+                        <th>Título</th>
+                        <th>Nombre del Rector</th>
+                        <th>Campus y Fecha de Expedición de título</th>
+                        <th>Libro</th>
+                        <th>Foja</th>
+                        <th>CURP</th>
+                        <th>Estudios de Bachillerato</th>
+                        <th>Periodo</th>
+                        <th>Entidad Federativa</th>
+                        <th>Institución</th>
+                        <th>Carrera</th>
+                        <th>Periodo</th>
+                        <th>Entidad Federativa</th>
+                        <th>Fecha de Examen</th>
+                        <th>Lugar y fecha de la Certificación</th>
+                        <th>Administración Escolar</th>
 
-	</div>
-	
+                      </tr>
+                    </thead>
 
+                    <tbody>
+                      <?php  
+                    		$sql = "SELECT * FROM titulacion";
+                    		$result = $conexion->consulta($sql);
+
+                    		while($row = mysqli_fetch_array($result)){
+                          $count = $count + 1;
+                    			$nombre = $row['nombre']." ".$row['a_paterno']." ".$row['a_materno'];
+                    			ECHO "<tr>
+                    					<td > $count</td>
+                    					<td > $row[tipo_titulacion]</td>
+                    					<td > $nombre</td>
+                    					<td > $row[titulo]</td>
+                    					<td > $row[nombre_rector]</td>
+                    					<td > $row[exp_titulo]</td>
+                    					<td > $row[libro]</td>
+                    					<td > $row[foja]</td>
+                    					<td > $row[curp]</td>
+                    					<td > $row[nombre_bachillerato]</td>
+                    					<td > $row[periodo_bachillerato]</td>
+                    					<td > $row[entidad_bachillerato]</td>
+                    					<td > $row[institucion]</td>
+                    					<td > $row[carrera]</td>
+                    					<td > $row[periodo_carrera]</td>
+                    					<td > $row[entidad_institucion]</td>
+                    					<td > $row[examen_profesional]</td>
+                    					<td > $row[certificacion]</td>
+                    					<td > $row[administracion_escolar]</td>
+                    				</tr>";
+                    		}
+                    	?>                
+                    </tbody>
+          </table>
+      </div>
+	   </div>
 
 </body>
+
 </html>
